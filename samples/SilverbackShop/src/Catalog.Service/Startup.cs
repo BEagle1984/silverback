409227191828f1ea4f0e1117c1457ab34c412896
BEagle1984 <aquilinis@outlook.com>
2018-05-20 10:11:15 +0200

    Introduce Sqllite in Catalog.Service

--- samples/SilverbackShop/src/Catalog.Service/Startup.cs
+++ samples/SilverbackShop/src/Catalog.Service/Startup.cs
@@ -28,7 +28,7 @@ namespace SilverbackShop.Catalog.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CatalogContext>(o => o.UseInMemoryDatabase("CatalogContext"));
+            services.AddDbContext<CatalogContext>(o => o.UseSqlite(@"Data Source=Data\Catalog.db"));
 
             services.AddMvc();
             services.AddSwaggerGen(c =>
