22291ddbc54ec42d4582b1a2c561e88fd89b8416
ppx80 <fmichelucci@gmail.com>
2018-05-20 17:12:52 +0200

    Added commit features in the consumer

--- silverback-integration-kafka/src/Silverback.Integration.Kafka/Messaging/Broker/KafkaConsumer.cs
+++ silverback-integration-kafka/src/Silverback.Integration.Kafka/Messaging/Broker/KafkaConsumer.cs
@@ -2,6 +2,8 @@
 using Confluent.Kafka;
 using Confluent.Kafka.Serialization;
 using Silverback.Messaging.Messages;
+using Common.Logging.Factory;
+using Common.Logging;
 
 namespace Silverback.Messaging.Broker
 {    
@@ -11,34 +13,43 @@ namespace Silverback.Messaging.Broker
         private bool _disconnected = false;
         private readonly KafkaEndpoint _endpoint;
         private Consumer<byte[], byte[]> _consumer;
+        private readonly ILog _log;
+        
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="KafkaConsumer"/> class.
-        /// </summary>
-        /// <param name="endpoint">The endpoint.</param>
-        /// <param name="consumer">The consumer.</param>
-        public KafkaConsumer(KafkaBroker broker, KafkaEndpoint endpoint) :
+        /// <inheritdoc />
+        public KafkaConsumer(IBroker broker, KafkaEndpoint endpoint) :
             base(broker,endpoint)
         {
             _endpoint = endpoint;
+            _log = LogManager.GetLogger<KafkaConsumer>();
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Gets a value indicating whether this instance is autocommit enabled.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is autocommit enabled; otherwise, <c>false</c>.
+        /// </value>
+        private bool IsAutocommitEnabled =>
+            _endpoint.Configuration.ContainsKey("enable.auto.commit") && (bool)_endpoint.Configuration["enable.auto.commit"];
+
         internal void Connect()
         {
+            if (_consumer != null) return;
+
             _consumer = new Consumer<byte[], byte[]>(_endpoint.Configuration,
                 new ByteArrayDeserializer(),
                 new ByteArrayDeserializer());
 
             _consumer.OnPartitionsAssigned += (_, partitions) =>
             {
-                // TODO: Log.Trace($"Assigned partitions: [{string.Join(", ", partitions)}], member id: {consumer.MemberId}");
+                _log.Trace($"Assigned partitions: [{string.Join(", ", partitions)}], member id: {_consumer.MemberId}");
                 _consumer.Assign(partitions);
             };
 
             _consumer.OnPartitionsRevoked += (_, partitions) =>
             {
-                // TODO: Log.Trace($"Revoked partitions: [{string.Join(", ", partitions)}]");
+                _log.Trace($"Revoked partitions: [{string.Join(", ", partitions)}]");
                 _consumer.Unassign();
             };
 
@@ -46,35 +57,25 @@ namespace Silverback.Messaging.Broker
             
             while (!_disconnected)
             {
-                if (!_consumer.Consume(out var msg, TimeSpan.FromMilliseconds(100)))
+                if (!_consumer.Consume(out var msg, TimeSpan.FromMilliseconds(_endpoint.TimeoutPollBlock)))
                     continue;
 
                 HandleMessage(msg.Value);
-                if (!IsAutocommitEnabled)
-                {
-                    var committedOffsets = _consumer.CommitAsync(msg).Result;
-                    // TODO: Log.Trace($"Committed offset: {committedOffsets}");
-                }
+                if (IsAutocommitEnabled) continue;
+                if (msg.Offset % _endpoint.CommitOffsetEach != 0) continue;
+                var committedOffsets = _consumer.CommitAsync(msg).Result;
+                _log.Trace($"Committed offset: {committedOffsets}");
             }
         }
 
-        /// <inheritdoc />
         internal void Disconnect()
         {
-            Disconnected = true;
+            _disconnected = true;
             _consumer.Unassign();
             _consumer.Unsubscribe();
         }
 
-        private bool IsAutocommitEnabled =>
-            _endpoint.Configuration.ContainsKey("enable.auto.commit") ?
-            (bool)_endpoint.Configuration["enable.auto.commit"] :
-            false;
-
-        /// <summary>
-        /// Releases unmanaged and - optionally - managed resources.
-        /// </summary>
-        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        /// <inheritdoc/>
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -82,10 +83,7 @@ namespace Silverback.Messaging.Broker
                 Disconnect();
                 _consumer.Dispose();
             }
-
             base.Dispose(disposing);
         }
-
-        
     }
 }
