236ee8780dab42295cd7732bdcae6d3b393c9f60
ppx80 <fmichelucci@gmail.com>
2018-05-27 20:34:14 +0200

    async subscriber

--- silverback-integration-kafka/examples/Producer/Subscriber.cs
+++ silverback-integration-kafka/examples/Producer/Subscriber.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Messages;
 using Silverback.Messaging.Subscribers;
 
@@ -10,6 +12,16 @@ namespace Producer
     {
         public override void Handle(TestMessage message)
         {
+            Console.WriteLine($"Message '{message.Id}' delivered successfully!");
+        }
+    }
+
+
+    public class SubscriberAsync : AsyncSubscriber<TestMessage>
+    {
+        public override async Task HandleAsync(TestMessage message)
+        {
+            await Task.Delay(2000);
             Console.WriteLine($"Message '{message.Id}' delivered successfully!");
         }
     }
