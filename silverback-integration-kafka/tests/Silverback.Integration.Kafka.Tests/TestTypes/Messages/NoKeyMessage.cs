bbb6be2d056ccbad6102a89848541c7044a9e8c5
BEagle1984 <aquilinis@outlook.com>
2019-01-04 09:01:59 +0100

    Thread-safe DbInboundLog

--- silverback-integration-kafka/tests/Silverback.Integration.Kafka.Tests/TestTypes/Messages/NoKeyMessage.cs
+++ silverback-integration-kafka/tests/Silverback.Integration.Kafka.Tests/TestTypes/Messages/NoKeyMessage.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) 2018 Sergio Aquilini
+// This code is licensed under MIT license (see LICENSE file for details)
+
+using System;
 using Silverback.Messaging.Messages;
 
 namespace Silverback.Integration.Kafka.Tests.TestTypes.Messages
