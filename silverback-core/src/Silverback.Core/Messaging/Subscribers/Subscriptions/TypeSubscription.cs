e79d9281575092b45092658f24bc591e539c6394
BEagle1984 <aquilinis@outlook.com>
2019-03-19 10:44:58 +0100

    Cleanup

--- silverback-core/src/Silverback.Core/Messaging/Subscribers/Subscriptions/TypeSubscription.cs
+++ silverback-core/src/Silverback.Core/Messaging/Subscribers/Subscriptions/TypeSubscription.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
-using Silverback.Util;
 
 namespace Silverback.Messaging.Subscribers.Subscriptions
 {
