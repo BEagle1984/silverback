cd408838e1b8bf904dea0a19e6e006c33ce85ad9
aquilinis <sergio.aquilini@post.ch>
2019-12-19 12:40:01 +0100

    Display activity information in samples (+ switch to 1.1.0-rc2)

---
+++ samples/Examples/src/Silverback.Examples.Common/Logging/LoggerEnrichmentConfigurationExtensions.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2019 Sergio Aquilini
+// This code is licensed under MIT license (see LICENSE file for details)
+
+using System;
+using Serilog;
+using Serilog.Configuration;
+
+namespace Silverback.Examples.Common.Logging
+{
+    public static class LoggerEnrichmentConfigurationExtensions
+    {
+        public static LoggerConfiguration WithActivityId(this LoggerEnrichmentConfiguration enrichmentConfiguration)
+        {
+            if (enrichmentConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(enrichmentConfiguration));
+            }
+
+            return enrichmentConfiguration.With<ActivityIdEnricher>();
+        }
+    }
+}
