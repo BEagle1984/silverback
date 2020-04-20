b91f467109584d69856898cbf36f19a43e0ff652
BEagle1984 <aquilinis@outlook.com>
2020-04-20 15:29:16 +0200

    Improve message type resolution (JsonSerializer)
    * Make it fault tollerant to mismatching assembly version
    * Cache resolved types

--- src/Silverback.Integration/Messaging/Serialization/SerializationHelper.cs
+++ src/Silverback.Integration/Messaging/Serialization/SerializationHelper.cs
@@ -2,16 +2,37 @@
 // This code is licensed under MIT license (see LICENSE file for details)
 
 using System;
+using System.Collections.Concurrent;
 using Silverback.Messaging.Messages;
 
 namespace Silverback.Messaging.Serialization
 {
     internal static class SerializationHelper
     {
+        public static readonly ConcurrentDictionary<string, Type> TypesCache = new ConcurrentDictionary<string, Type>();
+        
         public static Type GetTypeFromHeaders<TDefault>(MessageHeaderCollection messageHeaders)
         {
             var typeName = messageHeaders.GetValue(DefaultMessageHeaders.MessageType);
-            return typeName != null ? Type.GetType(typeName) : typeof(TDefault);
+            if (string.IsNullOrEmpty(typeName))
+                return typeof(TDefault);
+
+            return TypesCache.GetOrAdd(typeName, _ =>
+            {
+                Type type = null;
+                try
+                {
+                    type = Type.GetType(typeName);
+                }
+                catch (Exception)
+                {
+                    // Ignore
+                }
+                
+                type ??= Type.GetType(CleanAssemblyQualifiedName(typeName));
+
+                return type;
+            });
         }
 
         public static IRawInboundEnvelope CreateTypedInboundEnvelope(
@@ -25,6 +46,16 @@ namespace Silverback.Messaging.Serialization
             typedInboundMessage.Message = deserializedMessage;
 
             return typedInboundMessage;
+        }
+
+        private static string CleanAssemblyQualifiedName(string typeAssemblyQualifiedName)
+        {
+            if (string.IsNullOrEmpty(typeAssemblyQualifiedName))
+                return typeAssemblyQualifiedName;
+
+            var split = typeAssemblyQualifiedName.Split(',');
+
+            return split.Length >= 2 ? $"{split[0]}, {split[1]}" : typeAssemblyQualifiedName;
         }
     }
 }
