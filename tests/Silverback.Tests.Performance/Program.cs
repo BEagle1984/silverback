2dc2b556ac39b0d8b01ed7c24c31e0e36fe2f510
Marc Sallin <marc.sallin@outlook.com>
2020-05-06 15:39:48 +0200

    Add BenchmarkDotnet test

---
+++ tests/Silverback.Tests.Performance/Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2020 Sergio Aquilini
+// This code is licensed under MIT license (see LICENSE file for details)
+
+using BenchmarkDotNet.Running;
+
+namespace Silverback.Tests.Performance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
