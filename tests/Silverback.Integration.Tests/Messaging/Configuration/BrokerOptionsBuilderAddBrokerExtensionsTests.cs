c20fc2b0c0ef0e3af8c76f692c5c375d1165706c
BEagle1984 <aquilinis@outlook.com>
2020-06-01 01:15:26 +0200

    Fix forgotten tests

---
+++ tests/Silverback.Integration.Tests/Messaging/Configuration/BrokerOptionsBuilderAddBrokerExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) 2020 Sergio Aquilini
+// This code is licensed under MIT license (see LICENSE file for details)
+
+using System.Linq;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Silverback.Messaging.Broker.Behaviors;
+using Silverback.Messaging.Diagnostics;
+using Silverback.Tests.Integration.TestTypes;
+using Xunit;
+
+namespace Silverback.Tests.Integration.Messaging.Configuration
+{
+    public class BrokerOptionsBuilderAddBrokerExtensionsTests
+    {
+        [Fact]
+        public void AddBroker_ActivityBehaviorsRegisteredForDI()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddNullLogger()
+                .AddSilverback()
+                .WithConnectionToMessageBroker(options => options.AddBroker<TestBroker>())
+                .Services.BuildServiceProvider();
+
+            var registeredBehaviors = serviceProvider.GetServices<IBrokerBehavior>().ToList();
+
+            registeredBehaviors.Should().Contain(behavior => behavior.GetType() == typeof(ActivityProducerBehavior));
+            registeredBehaviors.Should().Contain(behavior => behavior.GetType() == typeof(ActivityConsumerBehavior));
+        }
+
+        [Fact]
+        public void AddBroker_ExceptionLoggerBehaviorRegisteredForDI()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddNullLogger()
+                .AddSilverback()
+                .WithConnectionToMessageBroker(options => options.AddBroker<TestBroker>())
+                .Services.BuildServiceProvider();
+
+            var registeredBehaviors = serviceProvider.GetServices<IBrokerBehavior>().ToList();
+
+            registeredBehaviors.Should()
+                .Contain(behavior => behavior.GetType() == typeof(FatalExceptionLoggerConsumerBehavior));
+        }
+    }
+}
---
+++ tests/Silverback.Integration.Tests/Messaging/Configuration/SilverbackBuilderWithConnectionToExtensionsTests.cs
@@ -0,0 +1,24 @@
+// Copyright (c) 2020 Sergio Aquilini
+// This code is licensed under MIT license (see LICENSE file for details)
+
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Silverback.Messaging.Broker;
+using Xunit;
+
+namespace Silverback.Tests.Integration.Messaging.Configuration
+{
+    public class SilverbackBuilderWithConnectionToExtensionsTests
+    {
+        [Fact]
+        public void WithConnectionToMessageBroker_BrokerCollectionRegisteredForDI()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSilverback()
+                .WithConnectionToMessageBroker(options => { })
+                .Services.BuildServiceProvider();
+
+            serviceProvider.GetService<IBrokerCollection>().Should().NotBeNull();
+        }
+    }
+}
