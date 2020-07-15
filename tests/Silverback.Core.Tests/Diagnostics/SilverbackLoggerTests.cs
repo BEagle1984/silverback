c83349d07fd3c28aa0309007cb6dd3df594a788d
Eggimann Markus, I232 <markus.eggimann@post.ch>
2020-07-15 17:38:39 +0200

    Add tests for silverback logger.

---
+++ tests/Silverback.Core.Tests/Diagnostics/SilverbackLoggerTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Logging;
+using Silverback.Diagnostics;
+using Xunit;
+
+namespace Silverback.Tests.Core.Diagnostics
+{
+    public class SilverbackLoggerTests
+    {
+        [Fact]
+        public void Log_EmptyLogLevelMapping_LogLevelNotChanged()
+        {
+            var logLevelMapping = new LogLevelMapping();
+            var internalLogger = new LoggerSubstitute<object>();
+            var logger = new SilverbackLogger<object>(internalLogger, logLevelMapping);
+
+            logger.Log(LogLevel.Information, EventIds.KafkaConsumerConsumingMessage, "Log Message");
+
+            internalLogger.Received(LogLevel.Information, null, "Log Message");
+        }
+
+        [Fact]
+        public void Log_ConfiguredLogLevelMapping_LogLevelChanged()
+        {
+            var logLevelMapping = new LogLevelMapping
+            {
+                { EventIds.KafkaConsumerConsumingMessage, LogLevel.Error }
+            };
+            var internalLogger = new LoggerSubstitute<object>();
+            var logger = new SilverbackLogger<object>(internalLogger, logLevelMapping);
+
+            logger.Log(LogLevel.Information, EventIds.KafkaConsumerConsumingMessage, "Log Message");
+
+            internalLogger.Received(LogLevel.Error, null, "Log Message");
+        }
+
+        [Fact]
+        public void Log_EventIdNotConfigured_LogLevelNotChanged()
+        {
+            var logLevelMapping = new LogLevelMapping
+            {
+                { EventIds.KafkaConsumerConsumingMessage, LogLevel.Error }
+            };
+            var internalLogger = new LoggerSubstitute<object>();
+            var logger = new SilverbackLogger<object>(internalLogger, logLevelMapping);
+
+            logger.Log(LogLevel.Information, EventIds.KafkaConsumerConsumingCanceled, "Log Message");
+
+            internalLogger.Received(LogLevel.Information, null, "Log Message");
+        }
+    }
+}
---
+++ tests/Silverback.Core.Tests/Messaging/Configuration/SilverbackBuilderWithLogLevelsExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Silverback.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Silverback.Tests.Core.Messaging.Configuration
+{
+    public class SilverbackBuilderWithLogLevelsExtensionsTests
+    {
+        [Fact]
+        public void WithLogLevels_WithLogLevels_LogLevelMappingCorrectlyBuild()
+        {
+            var services = new ServiceCollection();
+
+            services
+                .AddNullLogger()
+                .AddSilverback()
+                .WithLogLevels(c => c
+                    .SetLogLevel(EventIds.BrokerConnected, LogLevel.Information)
+                    .SetLogLevel(EventIds.BrokerConnecting, LogLevel.Warning));
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            serviceProvider.GetRequiredService<ISilverbackLogger>().Should().NotBeNull();
+            serviceProvider.GetRequiredService<ISilverbackLogger<object>>().Should().NotBeNull();
+        }
+    }
+}
