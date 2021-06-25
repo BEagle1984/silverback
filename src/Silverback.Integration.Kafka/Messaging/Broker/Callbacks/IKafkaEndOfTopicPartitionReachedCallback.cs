d5eaf7fc0b62d36967182e179e7eacc03f303b98
Willimann Marco, I231 extern <marco.willimann@post.ch>
2021-06-25 16:14:37 +0200

    feat: add EndOfPartition callback

---
+++ src/Silverback.Integration.Kafka/Messaging/Broker/Callbacks/IKafkaEndOfTopicPartitionReachedCallback.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2020 Sergio Aquilini
+// This code is licensed under MIT license (see LICENSE file for details)
+
+using Confluent.Kafka;
+
+namespace Silverback.Messaging.Broker.Callbacks
+{
+    /// <summary>
+    ///     Declares the <see cref="OnEndOfTopicPartitionReached" /> event handler.
+    /// </summary>
+    public interface IKafkaEndOfTopicPartitionReachedCallback : IBrokerCallback
+    {
+        /// <summary>
+        ///     Called to report the end of partition has reached.
+        /// </summary>
+        /// <param name="topicPartition">
+        ///    The related topicPartition.
+        /// </param>
+        /// <param name="consumer">
+        ///    The related consumer.
+        /// </param>
+        void OnEndOfTopicPartitionReached(TopicPartition topicPartition, KafkaConsumer consumer);
+    }
+}
