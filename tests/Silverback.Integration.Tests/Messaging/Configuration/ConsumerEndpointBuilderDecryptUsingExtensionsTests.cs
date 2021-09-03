6059294e601a595c397cc805fd819686be51e0f8
BEagle1984 <aquilinis@outlook.com>
2021-09-03 18:54:26 +0200

    test: fix wrong usage of NSubstitute causing tests flakiness

--- tests/Silverback.Integration.Tests/Messaging/Configuration/ConsumerEndpointBuilderDecryptUsingExtensionsTests.cs
+++ tests/Silverback.Integration.Tests/Messaging/Configuration/ConsumerEndpointBuilderDecryptUsingExtensionsTests.cs
@@ -2,7 +2,6 @@
 // This code is licensed under MIT license (see LICENSE file for details)
 
 using FluentAssertions;
-using NSubstitute;
 using Silverback.Messaging.Configuration;
 using Silverback.Messaging.Encryption;
 using Silverback.Tests.Types;
@@ -28,15 +27,16 @@ namespace Silverback.Tests.Integration.Messaging.Configuration
         }
 
         [Fact]
-        public void DecryptUsingAes_WithCallback_EncryptionSettingsSet()
+        public void DecryptUsingAes_WithKeyProvider_EncryptionSettingsSet()
         {
             var builder = new TestConsumerEndpointBuilder();
 
+            var key = new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05 };
             var iv = new byte[] { 0x11, 0x12, 0x13, 0x14, 0x15 };
 
             byte[] DecryptionKeyCallback(string keyIdentifier)
             {
-                return iv;
+                return key;
             }
 
             TestConsumerEndpoint endpoint = builder.DecryptUsingAes(DecryptionKeyCallback, iv).Build();
@@ -45,10 +45,7 @@ namespace Silverback.Tests.Integration.Messaging.Configuration
             endpoint.Encryption.As<SymmetricDecryptionSettings>().Key.Should().BeNull();
             endpoint.Encryption.As<SymmetricDecryptionSettings>().InitializationVector.Should().BeSameAs(iv);
             endpoint.Encryption.As<SymmetricDecryptionSettings>().KeyProvider.Should().NotBeNull();
-#pragma warning disable CS8602 // Unit test
-            endpoint.Encryption.As<SymmetricDecryptionSettings>().KeyProvider(Arg.Any<string>())
-                .Should().NotBeNull().And.BeSameAs(iv);
-#pragma warning restore CS8602 // Unit test
+            endpoint.Encryption.As<SymmetricDecryptionSettings>().KeyProvider!("abc").Should().BeSameAs(key);
         }
     }
 }
