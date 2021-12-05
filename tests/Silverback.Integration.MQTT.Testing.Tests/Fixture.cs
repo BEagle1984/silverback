2c75896933ae22b4ce69ffc0ca523b06004e6091
BEagle1984 <aquilinis@outlook.com>
2021-12-05 13:25:26 +0100

    test: prepare empty Silverback.Integration.MQTT.Testing.Tests project

---
+++ tests/Silverback.Integration.MQTT.Testing.Tests/Fixture.cs
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 Sergio Aquilini
+// This code is licensed under MIT license (see LICENSE file for details)
+
+using Xunit;
+
+namespace Silverback.Tests.Integration.Mqtt.Testing;
+
+public class Fixture
+{
+    [Fact]
+    public void Test() => Assert.True(true);
+}
