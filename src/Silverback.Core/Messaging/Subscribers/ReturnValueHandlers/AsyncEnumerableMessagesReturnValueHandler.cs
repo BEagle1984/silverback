55e869e19c0fbffe8e3846ef910120e38bda9c94
Igor Giovannini <info@igorgiovannini.ch>
2021-12-13 11:40:15 +0100

    feat: implement ReturnValueHandler for IAsyncEnumerables

---
+++ src/Silverback.Core/Messaging/Subscribers/ReturnValueHandlers/AsyncEnumerableMessagesReturnValueHandler.cs
@@ -0,0 +1,64 @@
+// Copyright (c) 2020 Sergio Aquilini
+// This code is licensed under MIT license (see LICENSE file for details)
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Silverback.Messaging.Configuration;
+using Silverback.Messaging.Publishing;
+using Silverback.Util;
+
+namespace Silverback.Messaging.Subscribers.ReturnValueHandlers
+{
+    /// <summary>
+    ///     Handles the returned <see cref="IAsyncEnumerable{T}" /> republishing all the messages.
+    /// </summary>
+    public class AsyncEnumerableMessagesReturnValueHandler : IReturnValueHandler
+    {
+        private readonly IBusOptions _busOptions;
+
+        private readonly IPublisher _publisher;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="AsyncEnumerableMessagesReturnValueHandler" /> class.
+        /// </summary>
+        /// <param name="publisher">
+        ///     The <see cref="IPublisher" /> to be used to publish the messages.
+        /// </param>
+        /// <param name="busOptions">
+        ///     The <see cref="IBusOptions" /> that specify which message types have to be handled.
+        /// </param>
+        public AsyncEnumerableMessagesReturnValueHandler(IPublisher publisher, IBusOptions busOptions)
+        {
+            _publisher = publisher;
+            _busOptions = busOptions;
+        }
+
+        /// <inheritdoc cref="IReturnValueHandler.CanHandle" />
+        public bool CanHandle(object returnValue) =>
+            returnValue != null &&
+            returnValue.GetType().GetInterfaces().Any(
+                i => i.IsGenericType &&
+                     i.GetGenericTypeDefinition() == typeof(IAsyncEnumerable<>) &&
+                     _busOptions.MessageTypes.Any(
+                         messageType =>
+                             messageType.IsAssignableFrom(i.GenericTypeArguments[0])));
+
+        /// <inheritdoc cref="IReturnValueHandler.Handle" />
+        public void Handle(object returnValue)
+        {
+            Check.NotNull(returnValue, nameof(returnValue));
+
+            AsyncHelper.RunSynchronously(
+                () => ((IAsyncEnumerable<object>)returnValue).ForEachAsync(_publisher.Publish));
+        }
+
+        /// <inheritdoc cref="IReturnValueHandler.HandleAsync" />
+        public Task HandleAsync(object returnValue)
+        {
+            Check.NotNull(returnValue, nameof(returnValue));
+
+            return ((IAsyncEnumerable<object>)returnValue).ForEachAsync(_publisher.PublishAsync);
+        }
+    }
+}
