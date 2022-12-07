5c7dffb1800fe9e8d4ccbe8c3ae46485bf6693a5
BEagle1984 <sergio@aquilini.ch>
2022-12-07 20:27:37 +0100

    chore: remove obsolete samples

--- samples/Examples/src/Silverback.Examples.Common/Messages/IntegrationEventB.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) 2020 Sergio Aquilini
-// This code is licensed under MIT license (see LICENSE file for details)
-
-namespace Silverback.Examples.Common.Messages
-{
-    public class IntegrationEventB : IntegrationEvent
-    {
-    }
-}
