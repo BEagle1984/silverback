bc59d660221e79d3085d65fb5a8a364ac2808d90
BEagle1984 <sergio@aquilini.ch>
2022-12-27 09:51:30 +0100

    feat: remove Silverback.EventSourcing
    Might be reimplemented later

--- src/Silverback.EventSourcing/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿// Copyright (c) 2020 Sergio Aquilini
-// This code is licensed under MIT license (see LICENSE file for details)
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Silverback.EventSourcing.Tests")]
--- tests/Silverback.EventSourcing.Tests/TestTypes/DomainEvents/PersonDomainEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) 2020 Sergio Aquilini
-// This code is licensed under MIT license (see LICENSE file for details)
-
-namespace Silverback.Tests.EventSourcing.TestTypes.DomainEvents;
-
-public abstract class PersonDomainEvent
-{
-}
