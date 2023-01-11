4012e7fc2afc84fdaa3e6ba124fffa910dccf912
BEagle1984 <sergio@aquilini.ch>
2023-01-11 21:15:14 +0100

    chore: fix warnings

--- src/Silverback.Integration/Messaging/Sequences/AddToSequenceResult.cs
+++ src/Silverback.Integration/Messaging/Sequences/AddToSequenceResult.cs
@@ -6,18 +6,37 @@ namespace Silverback.Messaging.Sequences
     /// <summary>
     ///     Encapsulates the result of the <see cref="ISequence.AddAsync" /> method.
     /// </summary>
-    /// <param name="IsSuccess">
-    ///     A value indicating whether the operation was successful or not.
-    /// </param>
-    /// <param name="PushedStreamsCount">
-    ///     The number of streams that have been actually pushed.
-    /// </param>
-    public record AddToSequenceResult(bool IsSuccess, int PushedStreamsCount)
+    public record AddToSequenceResult
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="AddToSequenceResult" /> class.
+        /// </summary>
+        /// <param name="isSuccess">
+        ///     A value indicating whether the operation was successful or not.
+        /// </param>
+        /// <param name="pushedStreamsCount">
+        ///     The number of streams that have been actually pushed.
+        /// </param>
+        public AddToSequenceResult(bool isSuccess, int pushedStreamsCount)
+        {
+            IsSuccess = isSuccess;
+            PushedStreamsCount = pushedStreamsCount;
+        }
+
         /// <summary>
         ///     Gets a static instance representing a failed call to <see cref="ISequence.AddAsync" />.
         /// </summary>
         public static AddToSequenceResult Failed { get; } = new(false, -1);
+
+        /// <summary>
+        ///     Gets a value indicating whether the operation was successful or not.
+        /// </summary>
+        public bool IsSuccess { get; init; }
+
+        /// <summary>
+        ///     Gets the number of streams that have been actually pushed.
+        /// </summary>
+        public int PushedStreamsCount { get; init; }
 
         /// <summary>
         ///     Returns a new instance representing a successful call to <see cref="ISequence.AddAsync" />.
