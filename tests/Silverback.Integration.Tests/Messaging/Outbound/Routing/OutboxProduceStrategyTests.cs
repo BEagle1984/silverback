a724c61f5cd0d6785813d5c2d0cf6e2ae70ca8ff
Paul Cooke <paul.cooke@blueprism.com>
2023-02-20 08:11:28 +0000

    style: fixing style issue

--- tests/Silverback.Integration.Tests/Messaging/Outbound/Routing/OutboxProduceStrategyTests.cs
+++ tests/Silverback.Integration.Tests/Messaging/Outbound/Routing/OutboxProduceStrategyTests.cs
@@ -16,103 +16,106 @@ using Silverback.Tests.Integration.TestTypes;
 using Silverback.Tests.Logging;
 using Xunit;
 
-namespace Silverback.Tests.Integration.Messaging.Outbound.Routing;
-
-public class OutboxProduceStrategyTests
+namespace Silverback.Tests.Integration.Messaging.Outbound.Routing
 {
-    private readonly IServiceProvider _serviceProvider;
-    private readonly TransactionalOutboxBroker _mockTransactionalOutboxBroker;
-
-    private readonly IOutboundEnvelope _message1;
-    private readonly IOutboundEnvelope _message2;
-
-    public OutboxProduceStrategyTests()
+    public class OutboxProduceStrategyTests
     {
-        ServiceProvider serviceProvider = CreateServiceProviderRequiredForTransactionalOutboxBroker();
-        _mockTransactionalOutboxBroker = Substitute.For<TransactionalOutboxBroker>(Substitute.For<IOutboxWriter>(), serviceProvider);
+        private readonly IServiceProvider _serviceProvider;
 
-        var services = new ServiceCollection();
-        services
-            .AddScoped(_ => Substitute.For<IOutboundLogger<OutboxProduceStrategy>>())
-            .AddScoped(_ => _mockTransactionalOutboxBroker);
+        private readonly TransactionalOutboxBroker _mockTransactionalOutboxBroker;
 
-        _serviceProvider = services.BuildServiceProvider();
+        private readonly IOutboundEnvelope _message1;
 
-        _message1 = Substitute.For<IOutboundEnvelope>();
-        var endpoint1 = Substitute.For<IProducerEndpoint>();
-        _message1.Endpoint
-            .Returns(endpoint1);
-        endpoint1.Name
-            .Returns("endpoint1");
+        private readonly IOutboundEnvelope _message2;
 
-        _message2 = Substitute.For<IOutboundEnvelope>();
-        var endpoint2 = Substitute.For<IProducerEndpoint>();
-        _message2.Endpoint
-            .Returns(endpoint2);
-        endpoint2.Name
-            .Returns("endpoint2");
-    }
+        public OutboxProduceStrategyTests()
+        {
+            ServiceProvider serviceProvider = CreateServiceProviderRequiredForTransactionalOutboxBroker();
+            _mockTransactionalOutboxBroker = Substitute.For<TransactionalOutboxBroker>(Substitute.For<IOutboxWriter>(), serviceProvider);
 
-    [Fact]
-    public async Task ProduceAsync_WithMultipleMessagesWithDifferentEndpoints_ShouldFindAndProduceToCorrectProducers()
-    {
-        // Arrange
-        var outboxProduceStrategyImplementation = new OutboxProduceStrategy().Build(_serviceProvider);
-        var producerForMessage1 = Substitute.For<IProducer>();
-        _mockTransactionalOutboxBroker.GetProducer(_message1.Endpoint)
-            .Returns(producerForMessage1);
-        var producerForMessage2 = Substitute.For<IProducer>();
-        _mockTransactionalOutboxBroker.GetProducer(_message2.Endpoint)
-            .Returns(producerForMessage2);
+            var services = new ServiceCollection();
+            services
+                .AddScoped(_ => Substitute.For<IOutboundLogger<OutboxProduceStrategy>>())
+                .AddScoped(_ => _mockTransactionalOutboxBroker);
 
-        // Act
-        await outboxProduceStrategyImplementation.ProduceAsync(_message1);
-        await outboxProduceStrategyImplementation.ProduceAsync(_message2);
+            _serviceProvider = services.BuildServiceProvider();
 
+            _message1 = Substitute.For<IOutboundEnvelope>();
+            var endpoint1 = Substitute.For<IProducerEndpoint>();
+            _message1.Endpoint
+                .Returns(endpoint1);
+            endpoint1.Name
+                .Returns("endpoint1");
 
-        // Assert
-        await producerForMessage1.Received().ProduceAsync(_message1);
-        await producerForMessage2.Received().ProduceAsync(_message2);
+            _message2 = Substitute.For<IOutboundEnvelope>();
+            var endpoint2 = Substitute.For<IProducerEndpoint>();
+            _message2.Endpoint
+                .Returns(endpoint2);
+            endpoint2.Name
+                .Returns("endpoint2");
+        }
 
-    }
+        [Fact]
+        public async Task ProduceAsync_WithMultipleMessagesWithDifferentEndpoints_ShouldFindAndProduceToCorrectProducers()
+        {
+            // Arrange
+            var outboxProduceStrategyImplementation = new OutboxProduceStrategy().Build(_serviceProvider);
+            var producerForMessage1 = Substitute.For<IProducer>();
+            _mockTransactionalOutboxBroker.GetProducer(_message1.Endpoint)
+                .Returns(producerForMessage1);
+            var producerForMessage2 = Substitute.For<IProducer>();
+            _mockTransactionalOutboxBroker.GetProducer(_message2.Endpoint)
+                .Returns(producerForMessage2);
 
-    [Fact]
-    public async Task ProduceAsync_WithMultipleMessagesWithSameEndpoints_ShouldFindAndProduceToCorrectProducers()
-    {
-        // Arrange
-        var outboxProduceStrategyImplementation = new OutboxProduceStrategy().Build(_serviceProvider);
-        var producerForMessage1 = Substitute.For<IProducer>();
-        _mockTransactionalOutboxBroker.GetProducer(_message1.Endpoint)
-            .Returns(producerForMessage1);
-
-        var messageWithSameEndpoint = Substitute.For<IOutboundEnvelope>();
-        messageWithSameEndpoint.Endpoint
-            .Returns(messageWithSameEndpoint.Endpoint);
-
-        await outboxProduceStrategyImplementation.ProduceAsync(_message1);
-
-        // Act
-        _mockTransactionalOutboxBroker.ClearReceivedCalls();
-        await outboxProduceStrategyImplementation.ProduceAsync(messageWithSameEndpoint);
+            // Act
+            await outboxProduceStrategyImplementation.ProduceAsync(_message1);
+            await outboxProduceStrategyImplementation.ProduceAsync(_message2);
 
 
-        // Assert
-        await producerForMessage1.Received(1).ProduceAsync(_message1);
-        _mockTransactionalOutboxBroker.DidNotReceive().GetProducer(Arg.Any<IProducerEndpoint>());
-    }
+            // Assert
+            await producerForMessage1.Received().ProduceAsync(_message1);
+            await producerForMessage2.Received().ProduceAsync(_message2);
 
-    private static ServiceProvider CreateServiceProviderRequiredForTransactionalOutboxBroker()
-    {
-        var services = new ServiceCollection();
-        services.AddSilverback()
-            .WithConnectionToMessageBroker(
-                options => options
-                    .AddBroker<TestBroker>());
+        }
 
-        services.AddSingleton(Substitute.For<IHostApplicationLifetime>())
-            .AddLoggerSubstitute();
+        [Fact]
+        public async Task ProduceAsync_WithMultipleMessagesWithSameEndpoints_ShouldFindAndProduceToCorrectProducers()
+        {
+            // Arrange
+            var outboxProduceStrategyImplementation = new OutboxProduceStrategy().Build(_serviceProvider);
+            var producerForMessage1 = Substitute.For<IProducer>();
+            _mockTransactionalOutboxBroker.GetProducer(_message1.Endpoint)
+                .Returns(producerForMessage1);
 
-        var serviceProvider = services.BuildServiceProvider();
-        return serviceProvider;
+            var messageWithSameEndpoint = Substitute.For<IOutboundEnvelope>();
+            messageWithSameEndpoint.Endpoint
+                .Returns(messageWithSameEndpoint.Endpoint);
+
+            await outboxProduceStrategyImplementation.ProduceAsync(_message1);
+
+            // Act
+            _mockTransactionalOutboxBroker.ClearReceivedCalls();
+            await outboxProduceStrategyImplementation.ProduceAsync(messageWithSameEndpoint);
+
+
+            // Assert
+            await producerForMessage1.Received(1).ProduceAsync(_message1);
+            _mockTransactionalOutboxBroker.DidNotReceive().GetProducer(Arg.Any<IProducerEndpoint>());
+        }
+
+        private static ServiceProvider CreateServiceProviderRequiredForTransactionalOutboxBroker()
+        {
+            var services = new ServiceCollection();
+            services.AddSilverback()
+                .WithConnectionToMessageBroker(
+                    options => options
+                        .AddBroker<TestBroker>());
+
+            services.AddSingleton(Substitute.For<IHostApplicationLifetime>())
+                .AddLoggerSubstitute();
+
+            var serviceProvider = services.BuildServiceProvider();
+            return serviceProvider;
+        }
     }
 }
