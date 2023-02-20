05f6d55868dbc3dcb9dc705f0e769033391374eb
Paul Cooke <paul.cooke@blueprism.com>
2023-02-20 07:37:41 +0000

    style: fixing style issue

--- src/Silverback.Integration/Messaging/Outbound/TransactionalOutbox/OutboxProduceStrategy.cs
+++ src/Silverback.Integration/Messaging/Outbound/TransactionalOutbox/OutboxProduceStrategy.cs
@@ -1,13 +1,12 @@
 ﻿// Copyright (c) 2020 Sergio Aquilini
 // This code is licensed under MIT license (see LICENSE file for details)
-
+using System;
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Silverback.Diagnostics;
 using Silverback.Messaging.Broker;
 using Silverback.Messaging.Messages;
 using Silverback.Util;
-using System;
-using System.Threading.Tasks;
 
 namespace Silverback.Messaging.Outbound.TransactionalOutbox
 {
