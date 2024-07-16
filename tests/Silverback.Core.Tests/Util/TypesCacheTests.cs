4acebcf32362c54af5d2a3eb5ead5f15e7719994
biedermannn <nicolas.biedermann@post.ch>
2024-07-16 13:57:12 +0200

    improved parsing of assemblyQualifiedName

--- tests/Silverback.Core.Tests/Util/TypesCacheTests.cs
+++ tests/Silverback.Core.Tests/Util/TypesCacheTests.cs
@@ -62,5 +62,18 @@ namespace Silverback.Tests.Core.Util
 
             type.Should().Be(typeof(TestEventOne));
         }
+
+        [Theory]
+        [InlineData("Silverback.Tests.Core.TestTypes.Messages.TestEventOne2", "Silverback.Tests.Core.TestTypes.Messages.TestEventOne2")]
+        [InlineData("Silverback.Tests.Core.TestTypes.Messages.TestEventOne2, Silverback.Core.Tests", "Silverback.Tests.Core.TestTypes.Messages.TestEventOne2, Silverback.Core.Tests")]
+        [InlineData("Silverback.Tests.Core.TestTypes.Messages.TestEventOne2, Silverback.Core.Tests, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Silverback.Tests.Core.TestTypes.Messages.TestEventOne2, Silverback.Core.Tests")]
+        [InlineData("Silverback.Tests.Core.Util.TypesCacheTests+MyMessage`2[[System.Int32, System.Private.CoreLib, Version=5.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e],[System.Int64, System.Private.CoreLib, Version=5.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e]], Silverback.Core.Tests, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Silverback.Tests.Core.Util.TypesCacheTests+MyMessage`2, Silverback.Core.Tests")]
+        [InlineData("Silverback.Tests.Core.Util.TypesCacheTests+MyMessage`2[[System.Int32, System.Private.CoreLib, Version=5.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e],[System.Int64, System.Private.CoreLib, Version=5.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e]]", "Silverback.Tests.Core.Util.TypesCacheTests+MyMessage`2")]
+        public void GetType_GenericType_TypeReturned(string typeAssemblyQualifiedName, string expected)
+        {
+            string cleanedName = TypesCache.CleanAssemblyQualifiedName(typeAssemblyQualifiedName);
+
+            cleanedName.Should().Be(expected);
+        }
     }
 }
