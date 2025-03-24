fdb4a6d731aed87186eadd5d8842fdf5edc28702
BEagle1984 <sergio@aquilini.ch>
2025-03-24 20:17:13 +0100

    chore: update copyright year

--- src/Silverback.Integration/Messaging/Validation/MessageValidationMode.cs
+++ src/Silverback.Integration/Messaging/Validation/MessageValidationMode.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Sergio Aquilini
+// Copyright (c) 2025 Sergio Aquilini
 // This code is licensed under MIT license (see LICENSE file for details)
 
 namespace Silverback.Messaging.Validation;
