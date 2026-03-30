27fc9f75b6885c40af6baf8c5dc0e57db5606557
BEagle1984 <sergio@aquilini.ch>
2026-03-30 22:36:14 +0200

    docs: fix layout after docfx update

--- public/main.js
+++ public/main.js
@@ -1,1 +1,15 @@
-export default {}
+﻿export default {
+    defaultTheme: 'dark',
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/BEagle1984/silverback/',
+            title: 'View on GitHub'
+        },
+        {
+            icon: 'cloud-download',
+            href: 'https://www.nuget.org/packages?q=Silverback',
+            title: 'Download from NuGet'
+        }
+    ]
+}
